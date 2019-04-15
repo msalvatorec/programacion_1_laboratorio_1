f820935edac2423c66b19420e87c8074b9a70df8
alumno <alumno@LAB00PC00>
2019-04-15 19:57:04 -0300

    Ejemplo uso typedef struct

---
+++ Apuntes de clase/2019-04-15 Ejemplo uso struct (copia).c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*struct Persona
+{
+	char apellido [31];
+	char nombre [31];
+	int edad;
+	int isEmpty;
+};*/
+
+typedef struct
+{
+	char apellido[31];
+	char nombre[31];
+	int edad;
+	int isEmpty;
+}ePersona;
+
+int main()
+{
+    struct Persona per;
+    printf("\nIngrese apellido: ");
+    scanf("%s",per.apellido);
+
+    printf("\nIngrese nombre: ");
+    scanf("%s",per.nombre);
+
+    printf("\nIngrese edad: ");
+    scanf("%d",&per.edad);
+    per.isEmpty=0;
+
+    printf("\n\nApellido: %s \t",per.apellido);
+    printf("Nombre: %s \t ",per.nombre);
+    printf("Edad: %d \n\n",per.edad);
+
+
+    return 0;
+
+
+}
+
